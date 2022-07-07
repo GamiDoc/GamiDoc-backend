14dcd364ed7ff34ee97c590aded7006be7dfd857
andrea mangrella <mangrella.andrea@gmail.com>
2022-07-07 11:32:19 +0200

    enum to the papers

--- models/paper.js
+++ models/paper.js
@@ -17,23 +17,45 @@ const paperSchema = new mongoose.Schema({
   // Approved: {type: Boolean, default: false },
   Reviews: [{ type: Schema.Types.ObjectId ,required: true,ref: "Review" }],
   // Parametri divisi uno per uno 
-  // Transforma in enum quelli che dovrebbero essere enum fratm 
-  Aestetics:{ type: String, required: true },
-  Context:{ type: String, required: true },
-  Affordances:{ type: String, required: true },
-  Device:{ type: String, required: true },
-  Modality:{ type: String,required: true },
-  Feedback:{ type: String,required: true },
-  Rules:{ type: String, required: true },
+  Aestetics:{ 
+    type: String,
+    required: true 
+  },
+  Context:{ 
+    type: String, 
+    enum: [], // behavior
+    required: true 
+  },
+  Affordances:{ 
+    type: String, 
+    // ne servono un sacco 
+    required: true 
+  },
+  Device:{ 
+    type: String, 
+    enum: [],
+    required: true 
+  },
+  Modality:{ 
+    type: String,
+    enum: [],
+    required: true 
+  },
+  Feedback:{ 
+    type: String,
+    enum: [],
+    required: true 
+  },
+  Rules:{ 
+    type: String, 
+    enum: [],
+    required: true 
+  },
   Pdf: Buffer,
   // ultima modifica di un paper magari inutile perchè no patch dopo review, da vedere  
   // LastSave:{ type: Date, default: new Date() }
 }) 
 
-// paperSchema.virtual("tile_info").
-// get(function(){
-//   return { this.Title}
-// })
 
 // i controlli sulle variabili sarebbe meglio metterli qui come schemas, tipo controllo sul nome o sui contenuti dei campi 
 
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,14 @@ const express = require("express")
 
 const userRoutes = express.Router()
 
+const management = new ManagementClient({
+  grant_type: 'client_credentials',
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  domain: process.env.AUTH0_DOMAIN
+})
+
+
 // FirstConfig di 1 user 
 
 module.exports = userRoutes
